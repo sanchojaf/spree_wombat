f0b7b291b558dcc1256563efaee0a080ee0e895b
Miguel Sancho <sanchocuba@gmail.com>
2014-08-07 14:45:32 -0400

    add year and month to source payment serializer

--- app/serializers/spree/wombat/source_serializer.rb
+++ app/serializers/spree/wombat/source_serializer.rb
@@ -3,12 +3,20 @@ require 'active_model/serializer'
 module Spree
   module Wombat
     class SourceSerializer < ActiveModel::Serializer
-      attributes :name, :cc_type, :last_digits, :gateway_payment_profile_id, :gateway_customer_profile_id
+      attributes :name, :month, :year, :cc_type, :last_digits, :gateway_payment_profile_id, :gateway_customer_profile_id
  
       def gateway_customer_profile_id
         object.gateway_customer_profile_id
       end
  
+      def month
+        object.month
+      end
+      
+      def year
+        object.year
+      end
+
       def gateway_payment_profile_id
         object.gateway_payment_profile_id
       end

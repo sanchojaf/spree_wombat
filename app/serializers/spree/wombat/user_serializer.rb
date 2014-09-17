9bd3a58ac5a510a72f17f457175a2958ab3bf3bd
Miguel Sancho <sanchocuba@gmail.com>
2014-09-17 09:04:45 -0400

    add user serializer

---
+++ app/serializers/spree/wombat/user_serializer.rb
@@ -0,0 +1,26 @@
+require 'active_model/serializer'
+
+module Spree
+  module Wombat
+    # Accepts a Spree::StockItem and serializes this to the Hub Inventory format
+    class UserSerializer < ActiveModel::Serializer
+
+      attributes :id, :email, :firstname, :lastname
+      has_one :shipping_address, serializer: Spree::Wombat::AddressSerializer
+      has_one :billing_address, serializer: Spree::Wombat::AddressSerializer
+
+      def email
+        object.email
+      end
+
+      def firstname
+        object.first_name if object.first_name.present?
+      end
+
+      def lastname
+        object.last_name if object.last_name.present?
+      end
+
+    end
+  end
+end

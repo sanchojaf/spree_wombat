34fcdcd35d73c8ef9e6d889d80a6dc59ae4c4ca6
Miguel Sancho <sanchocuba@gmail.com>
2014-09-17 23:44:00 -0400

    add coments

--- lib/spree/wombat/handler/add_customer_handler.rb
+++ lib/spree/wombat/handler/add_customer_handler.rb
@@ -16,12 +16,16 @@ module Spree
           lastname = @payload["customer"]["lastname"]
 
           begin
+            puts "****************** @payload['customer']['shipping_address'] #{@payload['customer']['shipping_address']}"
             user.ship_address = Spree::Address.create!(prepare_address(firstname, lastname, @payload["customer"]["shipping_address"]))
+            puts "****************** @payload['customer']['billing_address] #{@payload['customer']['billing_address']}"
             user.bill_address = Spree::Address.create!(prepare_address(firstname, lastname, @payload["customer"]["billing_address"]))
+            
           rescue Exception => exception
             return response(exception.message, 500)
           end
-
+           
+          puts "*********** was sussesful the creation of address" 
           root_roles = @payload["customer"].delete(:roles)
           process_roles(user, root_roles)
           

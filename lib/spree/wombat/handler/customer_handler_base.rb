a16e4b51749a1121e1cf82ce5a94a21177914da8
Miguel Sancho <sanchocuba@gmail.com>
2014-09-17 22:22:24 -0400

    improve process address

--- lib/spree/wombat/handler/customer_handler_base.rb
+++ lib/spree/wombat/handler/customer_handler_base.rb
@@ -5,28 +5,19 @@ module Spree
 
         def prepare_address(firstname, lastname, address_attributes)
           
-          address_attributes['country'] = {
-            'iso' => address_attributes['country'].upcase }
+          address_hash[:country] = {
+            :iso => address_hash[:country].upcase }
 
-          if address_attributes['state'].length == 2
-            address_attributes['state'] = {
-              'abbr' => address_hash['state'].upcase }
+          if address_hash[:state].length == 2
+            address_hash[:state] = {
+              :abbr => address_hash[:state].upcase }
           else
-            address_attributes['state'] = {
-              'name' => address_attributes['state'].capitalize }
+            address_hash[:state] = {
+              :name => address_hash[:state].capitalize }
           end
-
-          country_iso = address_attributes.delete(:country)
-          country = Spree::Country.find_by_iso(country_iso)
-          raise Exception.new("Can't find a country with iso name #{country_iso}!") unless country_iso
-          address_attributes[:country_id] = country.id
-
-          state_name = address_attributes.delete(:state)
-          if state_name
-            state = Spree::State.find_by_name(state_name)
-            raise Exception.new("Can't find a State with name #{state_name}!") unless state
-            address_attributes[:state_id] = state.id
-          end
+          
+          ensure_country_id_from_params address_attributes
+          ensure_state_id_from_params address_attributes
 
           address_attributes[:firstname] = firstname
           address_attributes[:lastname] = lastname
@@ -46,6 +37,55 @@ module Spree
             user.roles << role unless user.roles.include?(role)
           end
         end
+        
+        
+        def self.ensure_country_id_from_params(address)
+          return if address.nil? or address[:country_id].present? or address[:country].nil?
+
+          begin
+            search = {}
+            if name = address[:country][:name]
+              search[:name] = name
+            elsif iso_name = address[:country][:iso_name]
+              search[:iso_name] = iso_name.upcase
+            elsif iso = address[:country][:iso]
+              search[:iso] = iso.upcase
+            elsif iso3 = address[:country][:iso3]
+              search[:iso3] = iso3.upcase
+            end
+
+            address.delete(:country)
+            address[:country_id] = Spree::Country.where(search).first!.id
+
+          rescue Exception => e
+            raise "Ensure order import address country: #{e.message} #{search}"
+          end
+        end
+
+        def self.ensure_state_id_from_params(address)
+          return if address.nil? or address[:state_id].present? or address[:state].nil?
+
+          begin
+            search = {}
+            if name = address[:state][:name]
+              search[:name] = name
+            elsif abbr = address[:state][:abbr]
+              search[:abbr] = abbr.upcase
+            end
+
+            address.delete(:state)
+            search[:country_id] = address[:country_id]
+
+            if state = Spree::State.where(search).first
+              address[:state_id] = state.id
+            else
+              address[:state_name] = search[:name] || search[:abbr]
+            end
+          rescue Exception => e
+            raise "Ensure order import address state: #{e.message} #{search}"
+          end
+        end
+        
 
       end
     end
